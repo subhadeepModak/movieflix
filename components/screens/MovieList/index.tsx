af83dad5c7c828f71523f22a6eeaae7d015de767
subhadeepModak <subhadeepmodak00@gmail.com>
2024-07-10 23:25:13 +0530

    final commit

--- components/screens/MovieList/index.tsx
+++ components/screens/MovieList/index.tsx
@@ -1,27 +1,46 @@
 /* eslint-disable react-native/no-inline-styles */
-import {SafeAreaView, View} from 'react-native';
-import React, {useEffect, useReducer, useState} from 'react';
+import {ActivityIndicator, SafeAreaView, View} from 'react-native';
+import React, {
+  useEffect,
+  useReducer,
+  useState,
+  useRef,
+  useCallback,
+} from 'react';
 import AppHeader from '../../headers';
 import styles from './styles';
 import Genre from '../../genre';
 import MovieCard from '../../common/Moviecard';
 import {genresApi, movieApi} from './api';
-import InfiniteScrollList from '../../common/InfiniteScrollList';
 import {
   INITIAL_FILTER_VALUE,
   filterReducer,
 } from './FilterReducer/filterReducer';
+import {FlatList} from 'react-native-bidirectional-infinite-scroll';
+import {debounce} from 'lodash';
 
 const formattedResult = (res: {results: any}, year: any) => {
-  return [
-    {id: year, value: year, type: 'label'},
-    {id: `${year}_blank`, value: '', type: 'label'},
-    ...res.results,
-  ];
+  if (res.results.length)
+    return [
+      {id: year, value: year, type: 'label'},
+      {id: `${year}_blank`, value: '', type: 'label'},
+      ...res.results,
+    ];
+  else return [];
 };
+
+const DEFAULT_START_PAGE = 2012;
+
 const MovieList = () => {
   const [filter, dispatch] = useReducer(filterReducer, INITIAL_FILTER_VALUE);
   const [genreList, setGenreList] = useState([]);
+  const [startPage, setStartPage] = useState(DEFAULT_START_PAGE);
+  const [endPage, setEndPage] = useState(DEFAULT_START_PAGE - 1);
+  const [loading, setLoading] = useState(false);
+  const [onStartReachedInProgress, setOnStartReachedInProgress] =
+    useState(false);
+  const [onEndReachedInProgress, setOnEndReachedInProgress] = useState(false);
+  const listRef = useRef(null);
 
   const loadGenres = async () => {
     try {
@@ -34,54 +53,87 @@ const MovieList = () => {
       console.log('Error on fetching Genres', err);
     }
   };
-  useEffect(() => {
-    if (genreList.length === 0) loadGenres();
-  }, []);
 
-  const handleLoadMovies = async (year: number, direction: string) => {
-    try {
-      const response = await movieApi({
-        page: '1',
-        sort_by: 'popularity.desc',
-        primary_release_year: year,
-        'vote_count.gte': 100,
-        with_genres: filter.with_genres.join(','),
-      });
-      const data = await response.json();
-      if (direction === 'up') {
-        const res = formattedResult(data, year);
+  const handleLoadMovies = useCallback(
+    debounce(async (direction: string) => {
+      if (loading) return;
+      setLoading(true);
 
-        const updatedList = [...res, ...filter.movieList];
-        dispatch({type: 'UPDATE_MOVIE_LIST', payload: updatedList});
-      } else {
-        const res = formattedResult(data, year);
-        const updatedList = [...filter.movieList, ...res];
-        dispatch({type: 'UPDATE_MOVIE_LIST', payload: updatedList});
+      try {
+        const page = direction === 'up' ? startPage - 1 : endPage + 1;
+        const response = await movieApi({
+          page: '1',
+          sort_by: 'popularity.desc',
+          primary_release_year: page,
+          'vote_count.gte': 100,
+          with_genres: filter.with_genres.join(','),
+        });
+
+        const data = await response.json();
+        const newItems = formattedResult(data, page);
+
+        console.log(page, newItems.length, direction);
+
+        if (direction === 'up') {
+          setStartPage(page);
+          dispatch({
+            type: 'UPDATE_MOVIE_LIST',
+            payload: [...newItems, ...filter.movieList],
+          });
+          setOnStartReachedInProgress(false);
+        } else {
+          setEndPage(page);
+          dispatch({
+            type: 'UPDATE_MOVIE_LIST',
+            payload: [...filter.movieList, ...newItems],
+          });
+          setOnEndReachedInProgress(false);
+        }
+      } catch (e) {
+        console.log('error fetching movies', e);
+      } finally {
+        setLoading(false);
       }
-      return true;
-    } catch (e) {
-      console.log('error fetching movies', e);
+    }, 1500),
+    [loading, startPage, endPage, filter],
+  );
+
+  const handleStartReached = () => {
+    console.log('start');
+    if (!onStartReachedInProgress || !onEndReachedInProgress) {
+      setOnStartReachedInProgress(true);
+      handleLoadMovies('up');
+      return Promise.resolve(true);
+    } else Promise.resolve(true);
+  };
+
+  const handleEndReached = () => {
+    console.log('end');
+
+    if (!onEndReachedInProgress || !onStartReachedInProgress) {
+      setOnEndReachedInProgress(true);
+      handleLoadMovies('down');
+      return Promise.resolve(true);
+    } else {
+      return Promise.resolve(true);
     }
   };
 
+  useEffect(() => {
+    if (genreList.length === 0) loadGenres();
+    if (filter.movieList.length === 0) handleLoadMovies('down');
+  }, []);
+
+  useEffect(() => {
+    handleLoadMovies('down');
+  }, [filter.with_genres]);
   const onGenreSelect = async (id: string) => {
+    setStartPage(DEFAULT_START_PAGE);
+    setEndPage(DEFAULT_START_PAGE - 1);
     if (filter.with_genres.includes(id)) {
-      await dispatch({
-        type: 'REMOVE_GENRES_FILTER',
-        payload: id,
-      });
-      const res = await handleLoadMovies(2012, 'down');
-      dispatch({
-        type: 'UPDATE_MOVIE_LIST',
-        payload: res,
-      });
+      await dispatch({type: 'REMOVE_GENRES_FILTER', payload: id});
     } else {
       await dispatch({type: 'ADD_GENRES_FILTER', payload: id});
-      const res = await handleLoadMovies(2012, 'down');
-      dispatch({
-        type: 'UPDATE_MOVIE_LIST',
-        payload: res,
-      });
     }
   };
 
@@ -94,20 +146,41 @@ const MovieList = () => {
         selectedGenres={filter.with_genres}
       />
       <SafeAreaView style={{flex: 1}}>
-        <InfiniteScrollList
-          fetchData={handleLoadMovies}
-          data={filter.movieList}
-          renderItem={({item}: any) => <MovieCard item={item} />}
-          numColumns={2}
-          contentContainerStyle={{
-            gap: 8,
-            paddingTop: 20,
-            paddingBottom: 30,
-            paddingHorizontal: 10,
-          }}
-          columnWrapperStyle={{gap: 8}}
-          defaultPage={2012}
-        />
+        {filter.movieList.length ? (
+          <FlatList
+            ref={listRef}
+            data={filter.movieList}
+            renderItem={({item}) => <MovieCard item={item} />}
+            keyExtractor={item => item.id.toString()}
+            onStartReached={handleStartReached}
+            onEndReached={handleEndReached}
+            showDefaultLoadingIndicators={true}
+            onStartReachedThreshold={10}
+            onEndReachedThreshold={10}
+            activityIndicatorColor={'white'}
+            HeaderLoadingIndicator={() =>
+              onStartReachedInProgress && (
+                <ActivityIndicator size="small" color="white" />
+              )
+            }
+            FooterLoadingIndicator={() =>
+              onEndReachedInProgress && (
+                <ActivityIndicator size="small" color="white" />
+              )
+            }
+            enableAutoscrollToTop={false}
+            columnWrapperStyle={{gap: 8}}
+            numColumns={2}
+            contentContainerStyle={{
+              gap: 8,
+              paddingTop: 20,
+              paddingBottom: 30,
+              paddingHorizontal: 10,
+            }}
+          />
+        ) : (
+          <ActivityIndicator size="small" color="white" />
+        )}
       </SafeAreaView>
     </View>
   );
